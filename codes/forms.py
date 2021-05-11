0eea2bf73dff6dfc22578eec85d2d7e41b8fec7a
Raúl Matias <63962174+RaulMatias7@users.noreply.github.com>
2021-05-11 07:23:08 +0100

    initial commit

---
+++ codes/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Code
+class CodeForm(forms.ModelForm):
+    number = forms.CharField(label='Code', help_text='Digite codigo de verificacao')
+    class Meta:
+        model = Code
+        fields = ('number',)
